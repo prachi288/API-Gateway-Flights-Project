66653553adb6aa6875c23806e23bd9540ba96311
prachi288 <200~96807804+prachi288@users.noreply.github.com>
2024-01-08 17:32:05 +0530

    added isAuthenticated middleware

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -3,7 +3,8 @@ const {UserController}=require('../../controllers');
 const {AuthRequestMiddlewares}=require('../../middlewares');
 const router = express.Router();
 console.log("Inside Routes");
-router.post('/signup',AuthRequestMiddlewares.validateAuthRequest,UserController.signup);
-router.post('/signin',AuthRequestMiddlewares.validateAuthRequest,UserController.signin);
+
+router.post('/signup',AuthRequestMiddlewares.validateAuthRequest, UserController.signup);
+router.post('/signin',AuthRequestMiddlewares.validateAuthRequest, UserController.signin);
 
 module.exports = router;
