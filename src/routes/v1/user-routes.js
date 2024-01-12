61818c545b481198eb150ea6a65d3defeb1509d9
prachi288 <200~96807804+prachi288@users.noreply.github.com>
2024-01-12 16:52:40 +0530

    added authorisation check for add role

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -6,5 +6,6 @@ console.log("Inside Routes");
 
 router.post('/signup',AuthRequestMiddlewares.validateAuthRequest, UserController.signup);
 router.post('/signin',AuthRequestMiddlewares.validateAuthRequest, UserController.signin);
+router.post('/role',AuthRequestMiddlewares.checkAuth, AuthRequestMiddlewares.isAdmin, UserController.addRoleToUser);
 
 module.exports = router;
